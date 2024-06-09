78cf1b4570417b82d0a3c03e66bf19a5b182f899
cutecat-vn <55250111+ttdat-thecodeguy@users.noreply.github.com>
2024-06-09 22:22:48 +0700

    features/login-register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./pages/chat-box/chat.component').then(m => m.ChatComponent) },
+    { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) }
+];
