ff3a6bb6f409edceafb7b11cc5f954635a7115fa
Full Name <ttdat8@tma.com.vn>
2024-06-18 11:19:21 +0700

    update register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,17 +2,41 @@ import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ButtonModule } from 'primeng/button';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { Tag } from '../../domains/tag.domain';
+
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
     InputTextModule,
     CheckboxModule,
-    ButtonModule 
+    ButtonModule,
+    MultiSelectModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  tags: Tag[] = [
+    {
+      id: 1,
+      name: 'Angular'
+    },
+    {
+      id: 2,
+      name: 'React'
+    },
+    {
+      id: 3,
+      name: 'Vue'
+    },
+    {
+      id: 4,
+      name: 'Angular'
+    }
+  ];
+  constructor() {
 
+  }
 }
